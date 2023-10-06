5f9620b0341f111a0c0fbac1d1279c7c5b9a7ae1
grigmer <144951803+grigmer@users.noreply.github.com>
2023-10-06 21:48:59 +0300

    четвертая лаба задача S1

--- lab4exS1.py
+++ lab4exS1.py
@@ -1,10 +1,13 @@
-s = input()
-
-count = 0
-
-for i in s:
+def numsum(s):
     
-    if "1"<= i and "9" >= i:
-        count += int(i)
+    count = 0
+    
+    for i in s:
+    
+        if "1"<= i and "9" >= i:
+            count += int(i)
+    
+    return count
 
-print(count)
+
+print(numsum(input()))
