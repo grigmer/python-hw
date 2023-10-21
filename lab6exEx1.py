cdc0f56d08e836d9a8a52a2efa7c40e72786ecd2
grigmer <144951803+grigmer@users.noreply.github.com>
2023-10-21 16:46:15 +0300

    шестая лаба задание Ex1

--- lab6exEx1.py
+++ lab6exEx1.py
@@ -1,7 +1,12 @@
 def flexible_get(a, b):
-    c = int(b)
+    c = int(b + 0.5)    #Округляем флотовый индекс до ближайшего целого
     try:
         return a[c]
     except IndexError:
         return None
-print(flexible_get([i for i in input()], float(input())))
+
+
+#Вызываем функцию для вводимых с клавиатуры элементов массива (в строку, через пробел) и индекса (в следующей строчке)
+#и печатаем результат ее работы.
+
+print(flexible_get([i for i in input().split()], float(input())))
